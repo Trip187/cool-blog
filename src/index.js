6cb006de3a659bc85d33ab5039117da3d6a8aef9
Ian Karari <iangkarari@gmail.com>
2025-11-05 11:41:21 +0300

    updates on img  url

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
-//basename={process.env.PUBLIC_URL}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
